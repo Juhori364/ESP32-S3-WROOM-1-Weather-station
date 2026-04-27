44f47b590dcc303441a32ba9328aefa91a15b14d
Juho <juho.rimpelainen@gmail.com>
2026-04-27 03:04:06 +0300

    Added functionalities for sending sensor values to db and displaying current values on OLED.

--- WiFitest/WiFitest.ino
+++ WiFitest/WiFitest.ino
@@ -1,11 +1,29 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <SPI.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME680.h>
 
-const char* ssid = "TP-Link_BA04";
-const char* password = "10171752";
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
 
 
-const char* serverName = "http://192.168.0.100:3000/upload";
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+#define SDA_PIN 9
+#define SCL_PIN 8
+Adafruit_BME680 bme;
+
+const char* ssid = "Juho's Galaxy A54 5G";
+const char* password = "hökäle04";
+
+
+const char* serverName = "http://192.168.36.77:3000/upload";
 
 
 unsigned long lastTime = 0;
@@ -13,6 +31,24 @@ unsigned long timerDelay = 5000;
 
 void setup() {
   Serial.begin(115200);
+  Wire.begin(SDA_PIN, SCL_PIN);
+ 
+
+  if (!bme.begin(0x76)) {
+    Serial.println("Sensoria ei löytynyt.");
+    while (1);
+  }
+
+  bme.setTemperatureOversampling(BME680_OS_8X);
+  bme.setHumidityOversampling(BME680_OS_2X);
+  bme.setPressureOversampling(BME680_OS_4X);
+  bme.setIIRFilterSize(BME680_FILTER_SIZE_3);
+  bme.setGasHeater(320, 150); // lämpötila, ms
+
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
 
   WiFi.begin(ssid, password);
   Serial.println("Connecting");
@@ -26,6 +62,34 @@ void setup() {
 }
 
 void loop() {
+
+  float temp = bme.readTemperature();
+ 
+  Serial.print("Lämpötila: ");
+  Serial.print(temp);
+  Serial.println(" °C");
+
+  char buf[16];
+  dtostrf(temp, 6, 2, buf);
+
+  display.clearDisplay();
+
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0, 0);
+  display.println(buf);
+
+  display.display(); // Always call this to push the buffer to the screen
+
+  delay(1000);
+
+
+
+
+
+
+
+
   if ((millis() - lastTime) > timerDelay) {
 
     if(WiFi.status()== WL_CONNECTED){
